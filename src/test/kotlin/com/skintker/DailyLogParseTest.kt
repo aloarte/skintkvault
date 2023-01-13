8c7269dd9fc7a0197dc605eb4ce2d21fdfaf4375
Albrath <aloarter@gmail.com>
2023-01-13 23:23:02 +0100

    Failing test fixed

--- src/test/kotlin/com/skintker/DailyLogParseTest.kt
+++ src/test/kotlin/com/skintker/DailyLogParseTest.kt
@@ -9,7 +9,7 @@ import kotlin.test.*
 class DailyLogParseTest {
 
     companion object {
-        const val jsonBody = "{\"dae\":\"2012-05-41\",\"foodList\":[\"food1\",\"food2\"],\"irritation\":{\"overallValue\":7,\"zoneValues\":[\"wrist\"]},\"additionalData\":{\"stressLevel\":10,\"weather\":{\"humidity\":7,\"temperature\":1},\"travel\":{\"traveled\":false,\"city\":\"Madrid\"},\"alcoholLevel\":\"FewWine\",\"beerTypes\":[\"Ale\"]}}"
+        const val jsonBody = "{\"date\":\"2012-05-41\",\"foodList\":[\"food1\",\"food2\"],\"irritation\":{\"overallValue\":7,\"zoneValues\":[\"wrist\"]},\"additionalData\":{\"stressLevel\":10,\"weather\":{\"humidity\":7,\"temperature\":1},\"travel\":{\"traveled\":false,\"city\":\"Madrid\"},\"alcoholLevel\":\"FewWine\",\"beerTypes\":[\"Ale\"]}}"
     }
 
     @Test
