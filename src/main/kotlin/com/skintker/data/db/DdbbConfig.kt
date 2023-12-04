b5ba44a7d11ff591b0ac654cae1ed3bbd654f3fe
Albrath <aloarter@gmail.com>
2023-12-04 20:18:45 +0100

    Fix linter issue

--- src/main/kotlin/com/skintker/data/db/DdbbConfig.kt
+++ src/main/kotlin/com/skintker/data/db/DdbbConfig.kt
@@ -1,3 +1,9 @@
 package com.skintker.data.db
 
-data class DdbbConfig(val userName:String, val password:String, val databaseName:String, val databasePort:String, val containerName:String)
+data class DdbbConfig(
+    val userName:String,
+    val password:String,
+    val databaseName:String,
+    val databasePort:String,
+    val containerName:String
+)
